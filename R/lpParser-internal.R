707bafad830f9288842f1db552f324b95c30e5bf
larsJonsen <lrj@sam.sdu.dk>
2014-03-30 15:15:23 +0200

    Addded R files
    Add all the files for the package

---
+++ R/lpParser-internal.R
@@ -0,0 +1,360 @@
+lpParser <- setClass(
+  'lpParser', 
+  slots = c(indexDim = 'numeric', 
+    indexLength = 'numeric', 
+    indexNames = 'character', 
+    indexList = 'list',
+    constDim = 'numeric',
+    constNames = 'character',
+    constList = 'list',
+    constIndex = 'list',
+    varDim = 'numeric',
+    varLength = 'numeric',
+    varNames = 'character',
+    varIndex = 'list',
+    varList = 'list',
+    eqDim = 'numeric',
+    eqLength = 'numeric',
+    eqNames = 'character',
+    # eqNamesIndex = 'character',
+    eqList = 'list',
+    eqIndex = 'list',
+    eqRhs = 'numeric',
+    eqLhs = 'matrix',
+    eqType = 'numeric',
+    obj = 'dsparseVector',
+    lplpParser = 'lpExtPtr',
+    status = 'numeric',
+    objective = 'numeric',
+    varVector = 'numeric',
+    varArrays = 'list',
+    boundsDim = 'numeric',
+    boundsList ='list',
+    sense = 'character',
+    type = 'numeric'
+  ),
+  prototype = list(indexDim = 1, 
+   indexLength = 1, 
+   indexNames = 'single', 
+   indexList = list(''),
+   constDim = 0,
+   constNames = NULL,
+   constList = list(),
+   constIndex= list(),
+   varDim = 0,
+   varLength = 0,
+   varNames = NULL,
+   varIndex = list(),
+   varList = list(),
+   eqDim = 0,
+   eqLength = 0,
+   eqNames = NULL,
+   # eqNamesIndex = NULL,
+   eqIndex = list(),
+   eqList = list(),
+   eqRhs = NULL,
+   eqLhs = NULL,
+   eqType = NULL,
+   obj = NULL,
+   lplpParser = NULL,
+   status = NULL,
+   objective = NULL,
+   varVector = NULL,
+   varArrays = NULL,
+   boundsDim = 0,
+   boundsList = NULL,
+   sense = 'min',
+   type = c('<=' = 1, '=<' = 1, '<' =1,
+    '>=' = 2, '=>' =  2, '>' = 2,
+    '=' = 3, '==' = 3))
+)
+
+
+.stopifnot <- function (..., text) {
+  n <- length(ll <- list(...))
+  if (n == 0L) 
+    return(invisible())
+  mc <- match.call()
+  for (i in 1L:n) if (!(is.logical(r <- ll[[i]]) && !any(is.na(r)) && 
+                          all(r))) {
+    ch <- deparse(mc[[i + 1]], width.cutoff = 60L)
+    if (length(ch) > 1L) 
+      ch <- paste(ch[1L], "....")
+    stop(paste(text,sprintf(ngettext(length(r), "%s is not TRUE", "%s are not all TRUE"), 
+                            ch), sep='\n'), call. = FALSE, domain = NA)
+  }
+  invisible()
+}
+
+.message = c("optimal solution found",
+             "the model is sub-optimal",
+             "the model is infeasible",
+             "the model is unbounded",
+             "the model is degenerate",
+             "numerical failure encountered",
+             "process aborted",
+             "timeout",
+             "the model was solved by presolve",
+             "the branch and bound routine failed",
+             "the branch and bound was stopped because of a break-at-first or break-at-value",
+             "a feasible branch and bound solution was found",
+             "no feasible branch and bound solution was found")
+
+.stSplit = function(string){
+  sign = 1
+  if (length(.minus(string)))  sign = -1
+  string = .rmPM(string)
+  if (0 < length(grep("[*]", string))) {
+    a = strsplit(string, '[\\*]')[[1]]
+    fac = gsub('[ ]','',a[1])
+    var = gsub('[ ]','',a[2])
+  } else {
+    fac = '1'
+    var = gsub('[ ]','',string)
+  }
+  return(c(sign = sign, factor = fac, variable = var))
+}
+
+
+.addSparseVectors <- function(x){
+  y = x[[1]]
+  n = length(x)
+  if(n > 1) for (i in seq(2,n)) y = y + x[[i]]
+  return(y)
+}
+
+.replace <- function(x, oldSlice, newSlice){
+  sapply(x, function(x) newSlice[match(x,oldSlice)])
+}
+
+.arrayReplace = function(array,vector){
+  D <- dim(array)
+  y <- vector[array]
+  dim(y) <- D
+  return(y)
+}
+
+.checkSolution = function(object){
+  if (is.null(object@status)) {
+    print('The model has to be solved first!')
+    return(FALSE)
+  }
+  if (object@status != 0) {
+    print('The model has not been solved sucsefully')
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+setGeneric('.makeMatrixPos',function(object, ...){standardGeneric('.makeMatrixPos')})
+
+setMethod(f = '.makeMatrixPos',
+  signature = 'lpParser',
+  definition = function(object, grid, sliceList, factItem, env){
+    with(as.list(factItem), {
+      y = object@eqLength + 1:nrow(grid)
+      # Constant
+      constNr = match(factor, object@constNames)
+      if (!is.na(constNr)){
+        # gridPos gives the position in the grid of intersection between eqIndex og constIndex
+        gridPos = which(object@eqIndex[[object@eqDim]] %in% object@constIndex[[constNr]])
+        # constPos gives the position in the constIndex which intersec with eqIndex and therfore is in grid
+        constPos = which(object@constIndex[[constNr]] %in% object@eqIndex[[object@eqDim]][gridPos])
+        # arrayPos gives a list of seqenses coresonding to contsIndex
+        arrayPos = lapply(object@indexLength[object@constIndex[[constNr]]], seq)
+        # For each line in the grid the values of the grid is replaced with the seq in the arrayPos
+        # and the proper values retrived from the list
+        val = apply(grid[,gridPos,drop = FALSE], 1, function(x) {
+          arrayPos[constPos] <- x
+          as.vector(do.call('[', c(list(object@constList[[constNr]]), arrayPos)))
+        }) * as.numeric(sign)
+        if (length(dim(val)) == 0){
+          dim(val) <- c(1,length(val))
+        }
+      } else {
+        .stopifnot(length(grep('[0-9]',factor)) & length((grep('[^0-9]',factor)))==0, 
+                   text = paste('Left hand side factor:', factor, '- not recornised 2'))
+        val = rep(eval(parse(text = factor)), length.out = nrow(grid)) * as.numeric(sign)
+        dim(val) <- c(1,length(val))
+      }
+      # variable
+      varStr = .getString(variable)
+      varSlice = .getSlice(variable)
+      varNr = match(varStr, object@varNames)
+      .stopifnot(!is.na(varNr), text = paste('Left hand variable:', variable, 'not recornsied'))
+      if (all(object@varIndex[[varNr]] == 1)){ #variable is 'single'
+        xPos = rep(object@varList[[varNr]], length.out = nrow(grid))
+        dim(xPos) = c(1,length(xPos))
+      } else {
+        # gridPos gives the position in the grid of intersection between eqIndex og constIndex
+        gridPos = which(object@eqIndex[[object@eqDim]] %in% object@varIndex[[varNr]])
+        if (!length(gridPos)){
+          # CHECK FOR INDEX
+          # if the grid pos is empty, ei no intersection between eqIndex og constIndex,
+          # the variable possition is return as a whole
+          xPos = apply(grid[,,drop = FALSE], 1, function(x) as.vector(object@varList[[varNr]]))
+        } else {
+          # modify grid by replcing index with the given slice
+          # CHECK LENGTH OF INDEX
+          varGrid <- grid
+          indexSlicePos = which(!is.na(sliceList))
+          if (!is.na(varSlice)) varGrid[,indexSlicePos] <- .replace(
+            varGrid[,indexSlicePos], 
+            eval(parse(text = sliceList[[indexSlicePos]]), envir = env),
+            eval(parse(text = varSlice), envir = env))
+          # varPos gives the position in the varIndex which intersec with eqIndex and therfore is in grid
+          varPos = which(object@varIndex[[varNr]] %in% object@eqIndex[[object@eqDim]][gridPos])
+          
+          # arrayPos gives a list of seqenses coresonding to varIndex
+          arrayPos = lapply(object@indexLength[object@varIndex[[varNr]]], seq)
+          # For each line in the grid the values of the grid is replaced with the seq in the arrayPos
+          # and the proper values retrived from the list
+          xPos = apply(varGrid[,gridPos,drop = FALSE], 1, function(x) {
+            arrayPos[varPos] <- x
+            as.vector(do.call('[',c(list(object@varList[[varNr]]),arrayPos)))
+          })
+          if (is.null(dim(xPos))) dim(xPos) = c(1,length(xPos))
+        }
+      }
+      n = nrow(xPos)
+      .stopifnot(nrow(val)<=n, 
+                 text = paste('Here is some problem: A constant:', factor, 'with dim', nrow(val),
+                              'have bigger dimensions than the variable:', variable, 'with dim', n))
+      cbind(x = as.vector(xPos),y = rep(y, each=n),val = as.vector(apply(val,2,rep, length.out=n)))
+    })
+  }
+)
+
+
+
+
+setGeneric('.getConstVal',function(object, ...){standardGeneric('.getConstVal')})
+
+setMethod(f = '.getConstVal',
+          signature = 'lpParser',
+          definition = function(object, grid, constNr){
+            if (length(object@constList[[constNr]]) == 1) {
+              return(rep(object@constList[[constNr]], length.out = nrow(grid)))
+            }
+            .stopifnot(object@constIndex[[constNr]] %in% object@eqIndex[[object@eqDim]],
+                       text = 'Constant index do not match equation index')
+            gridPos = match(object@constIndex[[constNr]], object@eqIndex[[object@eqDim]])
+            apply(grid[,gridPos,drop=F],1,function(x) do.call('[',c(list(object@constList[[constNr]]), x)))
+          }
+)
+
+setGeneric('.makeObjItems',function(object, ...){standardGeneric('.makeObjItems')})
+
+setMethod(f = '.makeObjItems',
+          signature = 'lpParser',
+          definition = function(object, factItem){
+            with(as.list(factItem), {
+              objItem = sparseVector(x=0, i =1, length=object@varLength)
+              # variable
+              varNr = match(variable, object@varNames)
+              .stopifnot(!is.na(varNr), text = paste('Left hand variable:', variable, 'not recornsied'))
+              a = cbind(index = object@varIndex[[varNr]], var = dim(object@varList[[varNr]]), const = 1)
+              if (nrow(a)==1) a = rbind(a, c(1,1,1))
+              # Constant
+              constNr = match(factor, object@constNames)
+              if (!is.na(constNr)){
+                # objPos gives the position in the objVarList of intersection between objIndex og constIndex
+                objPos = which(object@varIndex[[varNr]] %in% object@constIndex[[constNr]])
+                
+                constList = object@constList[[constNr]] * as.numeric(sign)
+                a[objPos,3] = a[objPos,2]
+              } else {
+                .stopifnot(length(grep('[0-9]',factor)) & length((grep('[^0-9]',factor)))==0, 
+                           text = paste('Constant factor:', factor, '- not recornised 2'))
+                constList = eval(parse(text = factor)) * as.numeric(sign)
+              }
+              dim(constList) = a[,3]
+              objItem[as.vector(object@varList[[varNr]])] = as.vector(.expandArray(constList, a[,2]))
+              return(objItem)
+            })
+          }
+)
+
+.expandArray <- function(x, newDim){
+  oldDim <- dim(x)
+  slice = mapply(function(old,new){
+    if (old==new) return(seq(old))
+    if (old==1) return(rep(1,new))
+    .stopifnot(FALSE, 'Dimension mismatch in .expandArray')},
+    oldDim, newDim)
+  do.call("[",c(list(x),slice))
+}
+
+setGeneric('.makeObjItems',function(object, ...){standardGeneric('.makeObjItems')})
+
+setMethod(f = '.makeObjItems',
+          signature = 'lpParser',
+          definition = function(object, factItem){
+            with(as.list(factItem), {
+              objItem = sparseVector(x=0, i =1, length=object@varLength)
+              # variable
+              varNr = match(variable, object@varNames)
+              .stopifnot(!is.na(varNr), text = paste('Left hand variable:', variable, 'not recornsied'))
+              a = cbind(index = object@varIndex[[varNr]], var = dim(object@varList[[varNr]]), const = 1)
+              if (nrow(a)==1) a = rbind(a, c(1,1,1))
+              # Constant
+              constNr = match(factor, object@constNames)
+              if (!is.na(constNr)){
+                # objPos gives the position in the objVarList of intersection between objIndex og constIndex
+                objPos = which(object@varIndex[[varNr]] %in% object@constIndex[[constNr]])
+                
+                constList = object@constList[[constNr]] * as.numeric(sign)
+                a[objPos,3] = a[objPos,2]
+              } else {
+                .stopifnot(length(grep('[0-9]',factor)) & length((grep('[^0-9]',factor)))==0, 
+                           text = paste('Constant factor:', factor, '- not recornised 2'))
+                constList = eval(parse(text = factor)) * as.numeric(sign)
+              }
+              dim(constList) = a[,3]
+              objItem[as.vector(object@varList[[varNr]])] = as.vector(.expandArray(constList, a[,2]))
+              return(objItem)
+            })
+          }
+)
+
+.expandArray <- function(x, newDim){
+  oldDim <- dim(x)
+  slice = mapply(function(old,new){
+    if (old==new) return(seq(old))
+    if (old==1) return(rep(1,new))
+    .stopifnot(FALSE, 'Dimension mismatch in .expandArray')},
+    oldDim, newDim)
+  do.call("[",c(list(x),slice))
+}
+
+.getParenArg <- function(string){
+  sSplit <- strsplit(string[1],'')[[1]]
+  substr(string, min(grep('[(]',sSplit)) + 1,
+         max(grep('[)]',sSplit))-1)
+}
+
+.getString = function(str) {
+  strsplit(str,'[[]')[[1]][1]
+}
+
+.getSlice = function(str){
+  strsplit(strsplit(str,'[[]')[[1]][2],'[]]')[[1]][1]
+}
+
+.pmPos <- function(string){
+  sSplit = strsplit(string,'')[[1]]
+  setdiff(grep('[-+]',strsplit(string,'')[[1]]),
+          unlist(apply(cbind(grep('[[]',sSplit),grep('[]]',sSplit)),
+                       1, function(x) x[1]:x[2])))
+}
+
+.rmPM <- function(string){
+  substr(string,max(0,.pmPos(string)+1),1000L)
+}
+
+.minus <- function(string){
+  sSplit = strsplit(string,'')[[1]]
+  setdiff(grep('[-]',strsplit(string,'')[[1]]),
+          unlist(apply(cbind(grep('[[]',sSplit),grep('[]]',sSplit)),
+                       1, function(x) x[1]:x[2])))
+}
---
+++ R/lpParser.R
@@ -0,0 +1,369 @@
+
+setGeneric("Equation<-",function(object, ...){standardGeneric("Equation<-")})
+
+setReplaceMethod(
+  f="Equation",
+  signature="lpParser",
+  definition=function(object, ...){
+    eqList <- (...)
+    env = parent.frame()
+    .stopifnot(is.character(eqList), text = 'Eqations have to be characters')
+    eqSplit <- strsplit(eqList,';')
+    .stopifnot(sapply(eqSplit,function(x) length(x)==2),
+              text = 'Each equation need one only semi colon')
+    #env <- environment()
+    for (eqString in eqSplit){
+      object@eqDim = object@eqDim +1
+      # Index
+      indexRaw <- unlist(
+        lapply(strsplit(.getParenArg(eqString[1]), ','),
+          function(x) gsub(' ','',x)))
+      indexList <- lapply(indexRaw, .getString)
+      sliceList <- lapply(indexRaw, .getSlice)
+      .stopifnot(indexList %in% object@indexNames, 
+                text = 'Indexs in equation are not declared as indexs')
+      
+      object@eqIndex[[object@eqDim]] = sort(match(indexList, object@indexNames))
+      ## Index grid
+      # Index grid gives all combinationions of the indexes in a grid
+      grid = expand.grid(mapply(function(x,y){
+          if (is.na(x)) return(seq(y))
+          return(eval(parse(text = x), envir = env))
+        },
+        sliceList,
+        object@indexLength[object@eqIndex[[object@eqDim]]]))
+      # Numbers of equations ei length of grid
+      eqNr = object@eqLength + 1:nrow(grid)
+            # equation name
+      eqNam <- strsplit(eqString[1],'[(]')[[1]][1]
+      object@eqNames = c(object@eqNames, eqNam)
+      namGrid = apply(grid,1,
+          function(x) mapply(function(x,y) y[x],
+            as.list(x), object@indexList[object@eqIndex[[object@eqDim]]]))
+      if (length(dim(namGrid))==0) dim(namGrid) = c(1,length(namGrid))
+      namGrid = t(namGrid)
+#       nameList = apply(namGrid, 1, 
+#                        function (x) paste(eqNam, paste(Map(as.character,x),collapse = '_'),sep ='_'))
+#       object@eqNamesIndex = c(object@eqNamesIndex, gsub(' ','_',nameList))
+      # equation index grid
+      namGrid = as.data.frame(namGrid)
+      names(namGrid) <- object@indexNames[object@eqIndex[[object@eqDim]]]
+      rownames(namGrid) <- eqNr
+      object@eqList[[eqNam]] = namGrid
+      # equation type
+      a <- grep('[<=>]', strsplit(eqString[2],'')[[1]])
+      typeStr <- substr(eqString[2], min(a), max(a))
+      .stopifnot(typeStr %in% names(object@type), 
+                text ='Eqution sign not recornised \n use <=. >= or ==')
+      object@eqType = c(object@eqType,  rep(object@type[typeStr], length.out = nrow(grid)))
+      # right hand side
+      rhsStr = gsub(' ', '', substr(eqString[2], max(a) + 1, 1000L))
+      ## CHECK FOR [] IN RHS
+      constNr = match(rhsStr ,object@constNames)
+      if (!is.na(constNr)){
+        object@eqRhs = c(object@eqRhs, .getConstVal(object, grid, constNr))
+      } else {
+        .stopifnot(is.numeric(eval(parse(text = rhsStr))), 
+                  text = 'Right hand side not constant or numeric')
+        object@eqRhs = c(object@eqRhs, rep(eval(parse(text = rhsStr)), length.out = nrow(grid))) 
+      }
+      # left hand side
+      lhsStr = substr(eqString[2], 0, min(a)-1)
+      p = .pmPos(lhsStr)
+      pp = cbind(c(0,p),c(p-1,1000L))
+      termList = apply(pp,1,function(x) substr(lhsStr,x[1],x[2]))
+      
+      factorList <- lapply(termList, .stSplit)
+      MatrixPos = lapply(factorList,function(x) .makeMatrixPos(object, grid, sliceList, x, env))
+      object@eqLhs = do.call(rbind, c(list(object@eqLhs), MatrixPos))
+      # update total number of eqations
+      object@eqLength = object@eqLength + nrow(grid) 
+    } 
+    return (object)
+  }
+)
+
+
+setGeneric("Variable<-",function(object, ...){standardGeneric("Variable<-")})
+
+setReplaceMethod(
+  f="Variable",
+  signature="lpParser",
+  definition=function(object, ...){
+    var <- (...)
+    Vname <- names(var)
+    n <- length(var)
+    for (i in seq(n)){
+      .stopifnot(var[[i]] %in% object@indexNames, 
+                text = 'Variable index not in the declared indexs') 
+      object@varDim = object@varDim + 1
+      object@varNames = c(object@varNames, Vname[i])
+      object@varIndex[[object@varDim]] = sort(match(var[[i]], object@indexNames))
+      object@varList[[object@varDim]] = object@varLength +
+        array(1:prod(object@indexLength[object@varIndex[[object@varDim]]]),
+              object@indexLength[object@varIndex[[object@varDim]]])
+      
+      namGrid = expand.grid(object@indexList[object@varIndex[[object@varDim]]])
+      nameList = apply(namGrid,1, function(x) paste(Vname[i],paste(x,collapse = '_'),sep='_'))
+#       object@varNamesIndex = c(object@varNamesIndex,gsub(' ','_',nameList))
+      
+      object@varLength = object@varLength + 
+        prod(object@indexLength[object@varIndex[[object@varDim]]])
+    }
+    return (object)
+  }
+)
+
+
+setGeneric("Index<-",function(object, ...){standardGeneric("Index<-")})
+ 
+setReplaceMethod(
+  f="Index",
+  signature="lpParser",
+  definition=function(object, ...){
+    index <- (...)
+    Iname <- names(index)
+    n <- length(index)
+    for (i in seq(n)){
+      object@indexDim = object@indexDim +1
+      object@indexLength = c(object@indexLength, length(index[[i]]))
+      object@indexNames = c(object@indexNames, Iname[i])
+      object@indexList[[object@indexDim]] = index[[i]]}
+    return (object)
+    }
+)
+
+setGeneric("Constant<-",function(object, ...){standardGeneric("Constant<-")})
+
+setReplaceMethod(
+  f="Constant",
+  signature="lpParser",
+  definition=function(object, ...){
+    const <- (...)
+    Cname <- names(const)
+    n <- length(const)
+    for (i in seq(n)){
+      .stopifnot(const[[i]] %in% object@indexNames,
+                text = 'Constant index not in declared indexs ') 
+      object@constDim = object@constDim + 1
+      object@constNames = c(object@constNames, Cname[i])
+      object@constIndex[[object@constDim]] = sort(match(const[[i]], object@indexNames))
+      object@constList[[object@constDim]] = array(NA, object@indexLength[object@constIndex[[object@constDim]]])
+    }
+    return (object)
+  }
+)
+
+setGeneric("setConstant<-",function(object, ...){standardGeneric("setConstant<-")})
+
+setReplaceMethod(
+  f="setConstant",
+  signature="lpParser",
+  definition=function(object, ...){
+    const <- (...)
+    Cname <- names(const)
+    n <- length(const)
+    for (i in seq(n)){
+      .stopifnot(Cname[i] %in% object@constNames, 
+                text = 'Constant name not in the declared cosntants')
+      j <- match(Cname[i], object@constNames)
+      .stopifnot(dim(const[[i]]) == dim(object@constList[[j]]),
+                text = 'Constant diensions do not match the declared indexs dim') 
+      object@constList[[j]] <- const[[i]]
+    }
+    return (object)
+  }
+)
+
+
+  
+setGeneric('lhsMatrix',function(object, ...){standardGeneric('lhsMatrix')})
+
+setMethod(f = 'lhsMatrix',
+          signature = 'lpParser',
+          definition = function(object){
+            require(Matrix)
+            return(sparseMatrix(j = object@eqLhs[,1], i = object@eqLhs[,2], x= object@eqLhs[,3], 
+                                dims = c(object@eqLength, object@varLength)))
+          }
+)
+
+setGeneric('lhsImage',function(object, ...){standardGeneric('lhsImage')})
+
+setMethod(f = 'lhsImage',
+          signature = 'lpParser',
+          definition = function(object){
+            require(Matrix)
+            image(lhsMatrix(object))
+          }
+)
+
+setGeneric("Object<-",function(object, ...){standardGeneric("Object<-")})
+
+setReplaceMethod(
+  f="Object",
+  signature="lpParser",
+  definition=function(object, ...){
+    objStr <- (...)
+    .stopifnot(is.character(objStr), text = 'Objects have to be characters')
+    p = grep('[-+]',strsplit(objStr,'')[[1]])
+    pp = cbind(c(0,p),c(p-1,1000L))
+    termList = apply(pp,1,function(x) substr(objStr,x[1],x[2]))
+    factorList <- lapply(termList, .stSplit)
+    objList = lapply(factorList,function(x) .makeObjItems(object, x))
+    object@obj = .addSparseVectors(objList)
+    return(object)
+  }
+)
+
+setGeneric("Bounds<-",function(object, ...){standardGeneric("Bounds<-")})
+
+setReplaceMethod(
+  f="Bounds",
+  signature="lpParser",
+  definition=function(object, ...){
+    argList <- (...)
+    argNames = names(argList)
+    with(argList, {
+      if (!('variable' %in% argNames)) variable = NULL
+      if (!('lower' %in% argNames)) lower = NULL
+      if (!('upper' %in% argNames)) upper = NULL
+      if (!('columns' %in% argNames)) columns = NULL
+    
+      if (!is.null(variable)){
+        .stopifnot(is.null(columns), text = ' variables and colunmns can not be set at same time')
+        varNr = match(variable, object@varNames)
+        .stopifnot(!is.na(varNr), text = 'Variable not recornised')
+        columns = as.vector(object@varList[[1]])
+        
+      } else {
+        if (is.null(columns)) columns = 1:object@varLength
+      }
+      object@boundsDim = object@boundsDim + 1
+      object@boundsList[[object@boundsDim]] = list(
+        lower = suppressWarnings(rep(lower, length.out = length(columns))),
+        upper = suppressWarnings(rep(upper, length.out = length(columns))),
+        columns = columns)
+      return(object)
+    })
+  }
+)
+
+setGeneric("Solve",function(object, ...){standardGeneric("Solve")})
+
+setMethod(
+  f="Solve",
+  signature="lpParser",
+  definition=function(object, ...){
+    env = parent.frame()
+    objectName <- as.character(as.list(match.call())[['object']]) 
+    object@lplpParser = make.lp(nrow = object@eqLength, ncol = object@varLength)
+    set.rhs(object@lplpParser, object@eqRhs)
+    set.constr.type(object@lplpParser, object@eqType)
+    con = lhsMatrix(object)
+    for (i in seq(object@varLength)){
+      set.column(object@lplpParser, i,
+                 con@x[(con@p[i]+1):con@p[i+1]],
+                 indices=con@i[(con@p[i]+1):con@p[i+1]]+1)
+    }
+    set.objfn(object@lplpParser, object@obj@x, object@obj@i)
+    do.call(set.bounds, c(list(object@lplpParser),Bounds(object)))
+    lp.control(object@lplpParser, sense= object@sense)
+    object@status = solve(object@lplpParser)
+    cat(paste( 'Status rescived from lpSolve:', object@status, '\n ',
+               .message[object@status + 1]))
+    if (object@status==0){
+      primal = get.primal.solution(object@lplpParser)
+      object@objective = primal[1]
+      object@varVector = primal[(2+object@eqLength):length(primal)]
+      eqVal = primal[2:(1+object@eqLength)]
+      object@varArrays = lapply(object@varList, 
+                                .arrayReplace, object@varVector)
+      names(object@varArrays) = object@varNames
+      for (i in seq(along.with = object@varIndex)){
+        namList = lapply(object@varIndex[[i]], function(x){
+          return(object@indexList[[x]])})
+        names(namList) = object@indexNames[object@varIndex[[1]]]
+        dimnames(object@varArrays[[i]]) = namList}
+      
+      dual = get.dual.solution(object@lplpParser)
+      eqLambda = dual[2: (1+object@eqLength)]
+      for (i in seq(along = object@eqList)){
+        object@eqList[[i]]$value = eqVal[as.numeric(rownames(object@eqList[[i]]))]
+        object@eqList[[i]]$lambda = eqLambda[as.numeric(rownames(object@eqList[[i]]))]
+      }
+    }
+    # modify the origonal object 
+    # NOT THE R WAY !!
+    assign(objectName, object, envir = env)
+    # return the object invisible  
+    # for security if the output i assigned the R way
+    invisible(object)
+  }
+)
+
+
+setGeneric("getVariables",function(object, ...){standardGeneric("getVariables")})
+
+setMethod(
+  f="getVariables",
+  signature="lpParser",
+  definition=function(object, ...){
+    if (.checkSolution(object)) return(object@varArrays)
+    invisible(NULL)
+  }
+)
+
+setGeneric("getConstraints",function(object, ...){standardGeneric("getConstraints")})
+
+setMethod(
+  f="getConstraints",
+  signature="lpParser",
+  definition=function(object, ...){
+    if (.checkSolution(object)) return(object@eqList)
+    invisible(NULL)
+  }
+)
+
+
+setGeneric("Bounds",function(object, ...){standardGeneric("Bounds")})
+
+setMethod(
+  f="Bounds",
+  signature="lpParser",
+  definition=function(object, ...){
+    if(!is.null(object@status)) if(object@status == 0) return(get.bounds(object@lplpParser))
+    lower = rep(0, object@varLength)
+    upper = rep(Inf, object@varLength)
+  lapply(object@boundsList, function(x){
+    if (!is.null(x$lower)) lower[x$columns] <<- x$lower
+    if (!is.null(x$upper)) upper[x$columns] <<- x$upper
+  })
+  return(list(lower = lower, upper = upper))
+  }
+)
+
+setGeneric("Sense",function(object, ...){standardGeneric("Sense")})
+
+setMethod(
+  f="Sense",
+  signature="lpParser",
+  definition=function(object, ...){
+    env = parent.frame()
+    objectName <- as.character(as.list(match.call())[['object']])    
+    if (missing(...)) {
+      print(object@sense)
+    } else {
+      x <- (...)
+      .stopifnot(x %in% c('min','max'),
+              text ="Sense expect either 'min' or 'max' ")
+      object@sense = x
+       # modify the origonal object 
+      # NOT THE R WAY !!
+      assign(objectName, object, envir = env)
+    }
+    # return the object invisible  
+    # for security if the output i assigned the R way
+    invisible(object)
+  }
+)
